e4276198fab65fac72fc27bebb94f0b5d89a3770
mbaye diop <mbaye.diop@suite.sn>
2018-01-18 17:00:54 +0000

    addition of the content mail for validation of registering

--- common/models/etudiant.js
+++ common/models/etudiant.js
@@ -1,5 +1,27 @@
 'use strict';
 
-module.exports = function(Etudiant) {
+var config = require('../../server/config.json');
+var path = require('path');
+var senderAddress = "jobmbay@gmail.com"; //Replace this address with your actual address
 
+module.exports = function(Etudiant) {
+  Etudiant.afterRemote('create', function(context, user, next) {
+    var options = {
+      type: 'email',
+      to: user.email,
+      from: senderAddress,
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+      redirect: '/verified',
+      user: user
+    };
+
+    user.verify(options, function(err, response) {
+      if (err) {
+        Etudiant.deleteById(user.id);
+        return next(err);
+      }
+      next();
+    });
+  });
 };
