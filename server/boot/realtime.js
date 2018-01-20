42be097bf4cda3161fe79f70949fdf53c816d56c
mbaye diop <mbaye.diop@suite.sn>
2018-01-20 17:43:55 +0000

    d

--- server/boot/realtime.js
+++ server/boot/realtime.js
@@ -2,7 +2,7 @@
  * Created by jobmbay on 1/12/18.
  */
 /*var QRCode = require('qrcode')
-//get data as url
+//get qrcode data as url
 QRCode.toDataURL('Jobmbay is the name!', function (err, url) {
   console.log(url)
 })*/
